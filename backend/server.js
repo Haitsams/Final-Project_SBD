8c7e6a46d60211fd46cbc9e2ada39e79cba6ecec
Haitsams <harphypaw.09@gmail.com>
2026-05-17 22:25:07 +0700

    fixing

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,11 @@ const userRoutes = require('./routes/user.routes');
 const auctionRoutes = require('./routes/auction.routes');
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: '*', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use('/api/users', userRoutes);
 app.use('/api/auctions', auctionRoutes);
